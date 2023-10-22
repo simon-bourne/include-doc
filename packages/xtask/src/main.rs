0f5c8618c4ed616daec600885d55907a1235b13e
Simon Bourne <simonbourne@gmail.com>
2023-10-22 13:21:06 +0100

    Update xtask-base

--- packages/xtask/src/main.rs
+++ packages/xtask/src/main.rs
@@ -2,5 +2,5 @@ use xtask_base::{ci::CI, generate_open_source_files, CommonCmds};
 
 fn main() {
     let code_gen = |check| generate_open_source_files(2021, check);
-    CommonCmds::run(CI::standard_workflow(), code_gen)
+    CommonCmds::run(CI::standard_workflow(&[]), code_gen)
 }
